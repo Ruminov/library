71fd005c2e8b4eae220c6934c440161296624114
Ruminov <74424348+Ruminov@users.noreply.github.com>
2024-08-20 01:17:27 -0400

    complete the main and footer visual elements

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ function Book() {}
 function addBookToLibrary() {}
 
 function displayBook() {}
+
+// Auto update footer copyright year
+document.getElementById("copy-year").textContent = new Date().getFullYear();
