e412d6037e7baa6a222e0ec2f84e432a13148ae2
Ruminov <74424348+Ruminov@users.noreply.github.com>
2024-08-20 04:12:52 -0400

    Create modal window

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ function displayBook() {}
 
 // Auto update footer copyright year
 document.getElementById("copy-year").textContent = new Date().getFullYear();
+
+const modal = document.getElementById("modal");
+
+document.addEventListener("keypress", (e) => {
+  if (e.key === "i") modal.showModal();
+
+  if (e.key === "o") modal.close();
+});
