050f414935ce6f1df364f4d89b4fada3d1c6c2c6
Ruminov <74424348+Ruminov@users.noreply.github.com>
2024-08-25 11:33:59 -0400

    Fix change status button receptiveness in mobile

--- script.js
+++ script.js
@@ -129,15 +129,15 @@ myLibrary.forEach((card) => displayBook(card));
 function generateHTMLSnippet(obj) {
   return `<div class="book-image">
   <div class="book-status">
-  
-    <svg
+    <div class="status-book-button">
+      <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      class="status-book-button">
-      <title>Edit status</title>
-      <path
-        d="M0.41,13.41L6,19L7.41,17.58L1.83,12M22.24,5.58L11.66,16.17L7.5,12L6.07,13.41L11.66,19L23.66,7M18,7L16.59,5.58L10.24,11.93L11.66,13.34L18,7Z" />
-    </svg>
+      viewBox="0 0 24 24">
+        <title>Edit status</title>
+        <path
+          d="M0.41,13.41L6,19L7.41,17.58L1.83,12M22.24,5.58L11.66,16.17L7.5,12L6.07,13.41L11.66,19L23.66,7M18,7L16.59,5.58L10.24,11.93L11.66,13.34L18,7Z" />
+      </svg>
+    </div>
   </div>
   <div class="book-options">
    <button class="edit-book-button" title="Edit book">
@@ -251,7 +251,8 @@ catalog.addEventListener("click", (e) => {
     });
   }
   // Status button
-  if (e.target.matches("svg.status-book-button")) {
+  console.log(e.target);
+  if (e.target.matches("div.status-book-button")) {
     let cardIndex = e.target.closest("div.book-card").dataset.index;
     let statusContainer = e.target.closest("div.book-status");
 
